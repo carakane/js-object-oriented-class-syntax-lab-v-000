ba0f963a83a8a31b52e19697beda6f04b8cbccf5
Cara Kane <cara.kane@gmail.com>
2017-12-04 18:42:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ class BoardMember {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+class Ceo extendes BoardMember {
+  hireEmployee() {
+    return "Welcome aboard!"
+  }
+}
